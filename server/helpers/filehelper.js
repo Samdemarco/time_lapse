5465e5b2071f13814d6fca509220ece5e2ed2155
Samsonite <sam.demarco@gmail.com>
2023-02-23 16:00:58 -0500

    Update filehelper.js

--- server/helpers/filehelper.js
+++ server/helpers/filehelper.js
@@ -2,13 +2,26 @@ import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
 uuidv4();
 
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads');
+//     },
+//     filename: (req, file, cb) => {
+//         const fileName = uuidv4() + '-' + file.originalname
+//         cb(null,fileName);
+//     }
+// });
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads');
+        const uploadDir = './uploads';
+        if (!fs.existsSync(uploadDir)){
+            fs.mkdirSync(uploadDir);
+        }
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         const fileName = uuidv4() + '-' + file.originalname
-        cb(null,fileName);
+        cb(null, fileName);
     }
 });
 const filefilter = (req, file, cb) => {
